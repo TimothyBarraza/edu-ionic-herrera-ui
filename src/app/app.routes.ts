d4261378e09e293081022bc80890f076b29c1c05
Timothy <135766826+timothybarraza@users.noreply.github.com>
2023-11-03 16:43:19 -0500

    Se redireciona de manera automatica a inicio y ya no a home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
-  {
     path: 'inicio',
     loadComponent: () => import('./pages/inicio/inicio.page').then( m => m.InicioPage)
+  },
+  {
+    path: '',
+    redirectTo: 'inicio',
+    pathMatch: 'full',
   },
 ];

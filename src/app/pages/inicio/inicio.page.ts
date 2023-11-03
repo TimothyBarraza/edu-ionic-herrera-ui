ac081ca2c56e84da624a082f1bf41a4b5e26eb2d
Timothy <135766826+timothybarraza@users.noreply.github.com>
2023-11-03 18:42:36 -0500

    Se importo RouterLink en inicio.page.ts

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -2,13 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, RouterLink],
 })
 export class InicioPage implements OnInit {
 
